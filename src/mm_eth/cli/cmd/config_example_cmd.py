57a5ce7d9699540f7807b87533ba04394db4e5ba
mcbarinov <mcbarinov@proton.me>
2024-12-23 13:31:13 +0300

    remove cli

--- src/mm_eth/cli/cmd/config_example_cmd.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from mm_std import print_plain
-
-
-def run(command: str) -> None:
-    command = command.replace("-", "_")
-    example_file = Path(Path(__file__).parent.absolute(), "../config_examples", f"{command}.yml")
-    print_plain(example_file.read_text())
--- tests/cli/cmd/test_private_key_cmd.py
+++
@@ -1,8 +0,0 @@
-from mm_std import run_command
-
-
-def test_private_key_cmd(private_0, address_0):
-    cmd = f"mm-eth wallet private-key {private_0}"
-    res = run_command(cmd)
-    assert res.code == 0
-    assert res.stdout.strip().lower() == address_0.lower()
