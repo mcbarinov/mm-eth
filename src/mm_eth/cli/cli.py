5bdbca1241ac2edefa4e7d303b1a8c901e6ad722
mcbarinov <mcbarinov@proton.me>
2025-04-19 10:54:58 +0300

    process

---
+++ src/mm_eth/cli/cli.py
@@ -0,0 +1,55 @@
+import importlib.metadata
+from typing import Annotated
+
+import typer
+from mm_std import print_plain
+
+from mm_eth.account import DEFAULT_DERIVATION_PATH
+from mm_eth.cli.cmd.wallet import mnemonic_cmd, private_key_cmd
+
+app = typer.Typer(no_args_is_help=True, pretty_exceptions_enable=False, add_completion=False)
+
+wallet_app = typer.Typer(no_args_is_help=True, help="Wallet commands: generate mnemonic, private to address")
+app.add_typer(wallet_app, name="wallet")
+app.add_typer(wallet_app, name="w", hidden=True)
+
+
+@wallet_app.command(name="mnemonic", help="Generate eth accounts based on a mnemonic")
+def mnemonic_command(  # nosec
+    mnemonic: Annotated[str, typer.Option("--mnemonic", "-m")] = "",
+    passphrase: Annotated[str, typer.Option("--passphrase", "-p")] = "",
+    print_path: bool = typer.Option(False, "--print_path"),
+    derivation_path: Annotated[str, typer.Option("--path")] = DEFAULT_DERIVATION_PATH,
+    words: int = typer.Option(12, "--words", "-w", help="Number of mnemonic words"),
+    limit: int = typer.Option(10, "--limit", "-l"),
+    save_file: str = typer.Option("", "--save", "-s", help="Save private keys to a file"),
+) -> None:
+    mnemonic_cmd.run(
+        mnemonic,
+        passphrase=passphrase,
+        print_path=print_path,
+        limit=limit,
+        words=words,
+        derivation_path=derivation_path,
+        save_file=save_file,
+    )
+
+
+@wallet_app.command(name="private-key", help="Print an address for a private key")
+def private_key_command(private_key: str) -> None:
+    private_key_cmd.run(private_key)
+
+
+def version_callback(value: bool) -> None:
+    if value:
+        print_plain(f"mm-eth: {importlib.metadata.version('mm-eth')}")
+        raise typer.Exit
+
+
+@app.callback()
+def main(_version: bool = typer.Option(None, "--version", callback=version_callback, is_eager=True)) -> None:
+    pass
+
+
+if __name__ == "__main_":
+    app()
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import os
 import pytest
 from dotenv import load_dotenv
 from mm_crypto_utils import proxy
+from typer.testing import CliRunner
 
 load_dotenv()
 
@@ -27,6 +28,41 @@ def address_tether():
     return "0xdac17f958d2ee523a2206206994597c13d831ec7"
 
 
+@pytest.fixture()
+def mnemonic() -> str:
+    return "diet render mix evil relax apology hazard bamboo desert sign fence usage baby athlete cannon season busy ten jaguar silk rebel identify foster shrimp"  # noqa: E501
+
+
+@pytest.fixture
+def address_0():
+    return "0x10fd602Bff689e64D4720D1DCCCD3494f1f16623"
+
+
+@pytest.fixture
+def private_0():
+    return "0x7bb5b9c0ba991275f84b796b4d25fd3a8d7320911f50fade85410e7a2b000632"
+
+
+@pytest.fixture
+def address_1():
+    return "0x58487485c3858109f5A37e42546FE87473f79a4b"
+
+
+@pytest.fixture
+def private_1():
+    return "0xe4d16faffffa9b28adf02fb5f06998d174046c369d2daffe9a750fbe6a333417"
+
+
+@pytest.fixture
+def address_2():
+    return "0x97C77B548aE0d4925F5C201220fC6d8996424309"
+
+
+@pytest.fixture
+def private_2():
+    return "0xb7e0b671e176b04ceb0897a698d34771bfe9acf29273dc52a141be6e97145a00"
+
+
 @pytest.fixture(scope="session")
 def proxies() -> list[str]:
     return proxy.fetch_proxies_or_fatal_sync(os.getenv("PROXIES_URL"))
@@ -35,3 +71,8 @@ def proxies() -> list[str]:
 @pytest.fixture()
 def random_proxy(proxies: list[str]) -> str:
     return proxy.random_proxy(proxies)
+
+
+@pytest.fixture
+def cli_runner() -> CliRunner:
+    return CliRunner()
