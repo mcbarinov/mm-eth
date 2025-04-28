b895b0b7cc963d01c3d81334bee9a5fcd980c3ad
mcbarinov <mcbarinov@proton.me>
2025-04-28 13:22:46 +0300

    fix deploy_cmd

--- src/mm_eth/cli/cmd/deploy_cmd.py
+++ src/mm_eth/cli/cmd/deploy_cmd.py
@@ -1,5 +1,6 @@
-import yaml
-from mm_std import BaseConfig
+from typing import cast
+
+from mm_std import BaseConfig, print_console, toml_loads
 from pydantic import StrictStr
 
 from mm_eth import account, deploy, retry
@@ -9,10 +10,10 @@ from mm_eth.cli.cli_utils import BaseConfigParams
 class Config(BaseConfig):
     private_key: StrictStr
     nonce: int | None = None
-    gas: StrictStr
+    gas: int
     max_fee_per_gas: str
     max_priority_fee_per_gas: str
-    value: str | None = None
+    value: int | None = None
     contract_bin: StrictStr
     constructor_types: StrictStr = "[]"
     constructor_values: StrictStr = "[]"
@@ -29,8 +30,9 @@ async def run(cli_params: DeployCmdParams) -> None:
     if cli_params.print_config:
         config.print_and_exit({"private_key"})
 
-    constructor_types = yaml.full_load(config.constructor_types)
-    constructor_values = yaml.full_load(config.constructor_values)
+    parsed = toml_loads(f"constructor_types = {config.constructor_types}\nconstructor_values = {config.constructor_values}")
+    constructor_types = cast(list[str], parsed["constructor_types"])
+    constructor_values = cast(list[object], parsed["constructor_values"])
 
     sender_address = account.private_to_address(config.private_key).unwrap()
 
@@ -39,4 +41,5 @@ async def run(cli_params: DeployCmdParams) -> None:
             "can't get nonce"
         )
 
-    deploy.get_deploy_contract_data(config.contract_bin, constructor_types, constructor_values)
+    res = deploy.get_deploy_contract_data(config.contract_bin, constructor_types, constructor_values)
+    print_console(res)
